3738e02414df55ad312aaebd1262e9e86fd13bc6
RokRask <rokas.raskevic@gmail.com>
2024-12-31 11:08:03 +0200

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const mysql = require('mysql');
 
-const port = 6001;
+const port = 6457;
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -54,34 +54,40 @@ app.get('/read', (req, res) => {
 
 });
 
+
 app.post('/create', (req, res) => {
- 
+
     // INSERT INTO table_name (column1, column2, column3, ...)
     // VALUES (value1, value2, value3, ...);
- 
+
+    // DAryti saugiai, nes galima injekcija
+
     const sql = `
         INSERT INTO trees (name, height, type)
-        VALUES ('${req.body.name}', ${req.body.height}, '${req.body.type}')
+        VALUES (?, ?, ?)
     `;
- 
-    con.query(sql, (err, data) => {
+
+    con.query(sql, [req.body.name, req.body.height, req.body.type], (err) => {
         if (err) {
             res.send('Klaida įrašant duomenis');
             return;
         }
         res.send('OK');
     });
- 
- 
+
+
 });
 
+// DELETE FROM table_name WHERE condition;
+// DELETE FROM trees WHERE id = 123 OR 1;
+
 app.delete('/delete/:id', (req, res) => {
-   
+    
     // const sql = `
     //     DELETE FROM trees
     //     WHERE id = ${req.params.id}
     // `;
- 
+
     // con.query(sql, (err) => {
     //     if (err) {
     //         res.send('Klaida trinant duomenis');
@@ -89,14 +95,14 @@ app.delete('/delete/:id', (req, res) => {
     //     }
     //     res.send('OK');
     // });
- 
+
     // secure delete
- 
+
     const sql = `
         DELETE FROM trees
         WHERE id = ?
     `;
- 
+
     con.query(sql, [req.params.id], (err) => {
         if (err) {
             res.send('Klaida trinant duomenis');
@@ -104,6 +110,30 @@ app.delete('/delete/:id', (req, res) => {
         }
         res.send('OK');
     });
+
+
+});
+
+// UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
+
+app.put('/update/:id', (req, res) => {
+
+    const sql = `
+        UPDATE trees
+        SET height = ?, name = ?
+        WHERE id = ?
+    `;
+
+    con.query(sql, [req.body.height, req.body.name, req.params.id], (err) => {
+        if (err) {
+            res.send('Klaida atnaujinant duomenis');
+            return;
+        }
+        res.send('OK');
+    });
+
 });
 
 
--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 const readUl = document.querySelector('#read ul');
 
 const read = _ => {
-    axios.get('http://localhost:6001/read')
+
+    axios.get('http://localhost:6457/read')
         .then(res => {
-
             readUl.innerHTML = '';
-
             res.data.forEach(tree => {
                 const clone = document.querySelector('template').content.cloneNode(true);
                 const id = clone.querySelector('[data-id]');
@@ -22,31 +21,47 @@ const read = _ => {
             });
 
         });
+
 };
 
 const create = _ => {
     const name = document.querySelector('#create [data-name]').value;
     const height = parseFloat(document.querySelector('#create [data-height]').value);
     const type = document.querySelector('#create [data-type]').value;
- 
-    axios.post('http://localhost:6001/create', { name, height, type })
+
+    axios.post('http://localhost:6457/create', { name, height, type })
         .then(res => {
             console.log(res.data);
             read();
         });
 };
 
+
 const destroy = _ => {
- 
+
     const id = document.querySelector('#delete [data-id]').value;
- 
-    axios.delete('http://localhost:6001/delete/' + id)
+
+    axios.delete('http://localhost:6457/delete/' + id)
         .then(res => {
             console.log(res.data);
             read();
         });
-   
+    
 };
+
+const edit = _ => {
+    
+    const id = document.querySelector('#edit [data-id]').value;
+    const name = document.querySelector('#edit [data-name]').value;
+    const height = parseFloat(document.querySelector('#edit [data-water]').value);
+
+    axios.put('http://localhost:6457/update/' + id, { height, name })
+        .then(res => {
+            console.log(res.data);
+            read();
+        });
+    
+}
 
 read();
 
@@ -56,8 +71,18 @@ createButton.addEventListener('click', create);
 const destroyButton = document.querySelector('#delete button');
 destroyButton.addEventListener('click', destroy);
 
+const editButton = document.querySelector('#edit button');
+editButton.addEventListener('click', edit);
+
+
 const formHeight = document.querySelector('[data-height]');
 const formHeightShow = document.querySelector('[data-height-show]');
 formHeight.addEventListener('input', _ => {
     formHeightShow.innerText = parseFloat(formHeight.value).toLocaleString('lt-LT');
+});
+
+const formWater = document.querySelector('[data-water]');
+const formWaterShow = document.querySelector('[data-water-show]');
+formWater.addEventListener('input', _ => {
+    formWaterShow.innerText = parseFloat(formWater.value).toLocaleString('lt-LT');
 });
